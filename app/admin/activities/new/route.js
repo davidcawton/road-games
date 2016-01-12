139664d259bfd2bb3bf34869b9bf3462eaec681b
David Cawton <dcawton@gmail.com>
2016-01-12 15:11:07 -0600

    Activities CRUD work

--- app/admin/activities/new/route.js
+++ app/admin/activities/new/route.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model() {
+    return Ember.RSVP.hash({
+      activity: this.store.createRecord('activity'),
+      teams: this.store.findAll('team'),
+    });
+  },
+
+  actions: {
+    addNewActivity(formData) {
+      let newActivity = this.store.createRecord('activity');
+      newActivity.setProperties(formData);
+      newActivity.save().then(() => {
+        this.transitionTo('admin.activities');
+      });
+    },
+  },
 });
